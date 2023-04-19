173ed90d793a394a1cb228c1015ab10c2e2bb204
Cleankz <92687535+Cleankz@users.noreply.github.com>
2023-04-19 20:38:00 +0700

    Update parser.php

--- parser.php
+++ parser.php
@@ -24,6 +24,7 @@ function curlGetPage($url, $referer = 'https://www.google.com/') //url - адр�
 
 }
 $page =  curlGetPage(url:'https://meshok.net/item/134761649_%D0%9D%D0%BE%D0%B6_%D0%A1%D0%A1%D0%A1%D0%A0_%D0%B4%D0%BB%D1%8F_%D0%B7%D0%B0%D1%82%D0%BE%D1%87%D0%BA%D0%B8_%D1%82%D1%80%D1%83%D0%B1%D1%87%D0%B0%D1%82%D1%8B%D1%85_%D1%81%D0%B2%D0%B5%D1%80%D0%BB');
+
 $html = str_get_html($page);
 
 // foreach ($html->find('') AS $post){
